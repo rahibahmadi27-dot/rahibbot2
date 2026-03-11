e4c214634ad9455914a4c15f17e8d64086d787de
rahibahmadi27-dot <rahibahmadi27@gmail.com>
2026-03-11 22:55:13 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "9370102005" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "93701092005" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
